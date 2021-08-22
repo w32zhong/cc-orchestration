edc3ea44b9a8face5d4353d6b835786d6bf696ce
t-k- <clock126@126.com>
2021-08-22 19:58:47 -0400

    update sbatch-template.sh

--- sbatch-template.sh
+++ sbatch-template.sh
@@ -3,7 +3,7 @@
 #SBATCH --gres=gpu:1        # how many GPUs per node
 #SBATCH --cpus-per-task=2   # Cores proportional to GPUs: 6 on Cedar, 16 on Graham.
 #SBATCH --mem=32gb          # Memory proportional to GPUs: 32000 Cedar, 64000 Graham.
-#SBATCH --time=2-00:10      # 2 days and 10 minutes
+#SBATCH --time=4-02:10      # 4 days and 2 hours and 10 minutes
 #SBATCH --output=job-%j-%N.out
 
 export NCCL_BLOCKING_WAIT=1  # Set this variable to use the NCCL backend
@@ -13,4 +13,7 @@ export SBATCH_ACCOUNT=$SLURM_ACCOUNT
 export SALLOC_ACCOUNT=$SLURM_ACCOUNT
 
 set -x
-srun --unbuffered python pya0/utils/transformer.py 8 --master tcp://$(hostname):8921
+#srun python pytorch-test.py tcp://$(hostname):8921
+srun --unbuffered python pya0/utils/transformer.py 8 --master tcp://$(hostname):8921 --ckpoint base-models/bert-base-uncased
+#salloc --nodes=1 --gres=gpu:1 --cpus-per-task=2 --time=0-01:10
+#srun --jobid 12345 --pty bash
