70ff6cda1a9a6354f3de8fd9c64c37c4f77be79c
t-k- <clock126@126.com>
2021-08-23 02:01:45 -0400

    update sbatch-template.sh

--- sbatch-template.sh
+++ sbatch-template.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --nodes=4           # total nodes
+#SBATCH --nodes=16          # total nodes
 #SBATCH --gres=gpu:1        # how many GPUs per node
 #SBATCH --cpus-per-task=2   # Cores proportional to GPUs: 6 on Cedar, 16 on Graham.
 #SBATCH --mem=32gb          # Memory proportional to GPUs: 32000 Cedar, 64000 Graham.
@@ -13,12 +13,11 @@ export SBATCH_ACCOUNT=$SLURM_ACCOUNT
 export SALLOC_ACCOUNT=$SLURM_ACCOUNT
 
 set -x
-srun --unbuffered python pya0/utils/transformer.py 8 \
+srun --unbuffered python pya0/utils/transformer.py \
         --cluster tcp://$(hostname):8921 \
         --ckpoint base-models/bert-base-uncased \
         --tok_ckpoint base-models/bert-tokenizer \
-        --save_fold 100 --epochs 3
-
+        --batch_size 160 --save_fold 100 --epochs 3
 # Other example usages
 #srun python pytorch-test-v2.py tcp://$(hostname):8921
 #salloc --nodes=1 --gres=gpu:1 --cpus-per-task=2 --time=0-01:10
