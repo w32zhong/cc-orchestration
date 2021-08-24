384211a63c3f8b93336f268f1b72924c1b01edae
t-k- <clock126@126.com>
2021-08-24 01:37:02 -0400

    working p100 x 2 configuration.

--- sbatch-template.sh
+++ sbatch-template.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH --nodes=2           # total nodes
-#SBATCH --gres=gpu:4        # how many GPUs per node
-#SBATCH --cpus-per-task=2   # Cores proportional to GPUs: 6 on Cedar, 16 on Graham.
-#SBATCH --mem=32gb          # Memory proportional to GPUs: 32000 Cedar, 64000 Graham.
-#SBATCH --time=4-02:10      # 4 days and 2 hours and 10 minutes
+#SBATCH --gres=gpu:2        # how many GPUs per node
+#SBATCH --cpus-per-task=4   # Cores proportional to GPUs: 6 on Cedar, 16 on Graham.
+#SBATCH --mem=64gb          # Memory proportional to GPUs: 32000 Cedar, 64000 Graham.
+#SBATCH --time=6-02:10      # 4 days and 2 hours and 10 minutes
 #SBATCH --output=job-%j-%N.out
 
 export NCCL_BLOCKING_WAIT=1  # Set this variable to use the NCCL backend
@@ -17,7 +17,7 @@ srun --unbuffered python pya0/utils/transformer.py pretrain \
         --cluster tcp://$(hostname):8921 \
         --ckpoint base-models/bert-base-uncased \
         --tok_ckpoint base-models/bert-tokenizer \
-        --batch_size $((2 * 4 * 10)) --save_fold 100 --epochs 3
+        --batch_size $((2 * 2 * 8)) --save_fold 100 --epochs 3
 # Other example usages
 #srun python pytorch-test-v2.py tcp://$(hostname):8921
 #salloc --nodes=1 --gres=gpu:1 --cpus-per-task=2 --time=0-01:10 --mem=32gb
