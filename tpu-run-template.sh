1e97c55beae903db71554761fd19220b823a6420
t-k- <clock126@126.com>
2021-08-29 23:24:06 -0400

    update tpu-run-template.sh

--- tpu-run-template.sh
+++ tpu-run-template.sh
@@ -13,12 +13,18 @@
 # conda activate torch-xla-1.8.1
 # pip3 install fire GPUtil transformers
 # wget https://vault.cs.uwaterloo.ca/s/5t9N6wPtEn7pBrJ/download -O mse-aops-2021-vocab.pkl
-#
-# export TPU_IP_ADDRESS=`gcloud compute tpus describe mse --zone=europe-west4-a | grep -Po '(?<=ipAddress: ).*' | head -1` && echo $TPU_IP_ADDRESS
-# export XRT_TPU_CONFIG="tpu_worker;0;$TPU_IP_ADDRESS:8470"
+
+conda activate torch-xla-1.8.1
+export TPU_IP_ADDRESS=`gcloud compute tpus describe mse --zone=europe-west4-a | grep -Po '(?<=ipAddress: ).*' | head -1` && echo $TPU_IP_ADDRESS
+export XRT_TPU_CONFIG="tpu_worker;0;$TPU_IP_ADDRESS:8470"
+
+fallocate -l 20G /swapfile
+chmod 600 /swapfile
+mkswap /swapfile
+swapon /swapfile
 
 gcloud compute operations list
-# on a TPU-v3-8
+# on a TPU-v3-8 (torch-1.8)
 python3 pya0/utils/transformer.py pretrain \
 	--batch_size $((8 * 16)) \
 	--epochs 4 \
