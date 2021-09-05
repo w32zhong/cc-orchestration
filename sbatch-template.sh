02a43c898b00469be56a12c29ca822d6b6c0d0a3
t-k- <clock126@126.com>
2021-09-05 14:36:41 -0400

    more "automation" in sbatch-template.sh

--- sbatch-template.sh
+++ sbatch-template.sh
@@ -7,27 +7,54 @@
 #SBATCH --output=job-%j-%N.out
 
 set -x
+TRAINER=${1-colbert}
+DATA_VER=FBxsZSLMCeLZDMk
+CODE_VER=$(cd pya0 && pwd && git rev-parse HEAD)
+
+case $TRAINER in
+   pretrain)
+    DEV_BSIZE=12
+    EPOCHS=10
+    EXTRA_DAT=$DATA_DIR/mse-aops-2021-vocab.pkl
+    ;;
+   finetune)
+    DEV_BSIZE=12
+    EPOCHS=5
+    EXTRA_DAT=$DATA_DIR/mse-aops-2021-data.pkl.tags.ids
+    ;;
+   colbert)
+    DEV_BSIZE=10
+    EPOCHS=4
+    EXTRA_DAT=
+    EXTRA_ARG=--active_fp16
+    ;;
+esac
+
+START_POINT=$DATA_DIR/$TRAINER/model
+TOK_CKPOINT=$DATA_DIR/$TRAINER/tokenizer
+SHARDS_LIST=$DATA_DIR/shards-for-$TRAINER.txt
+
+N_NODE=$(cat $0 | grep -Po '(?<=SBATCH --nodes=)[0-9]+')
+N_GPUS=$(cat $0 | grep -Po '(?<=SBATCH --gres=gpu:)[0-9]+')
+
 export NCCL_BLOCKING_WAIT=1  # Set this variable to use the NCCL backend
 export SLURM_ACCOUNT=def-jimmylin
 export SBATCH_ACCOUNT=$SLURM_ACCOUNT
 export SALLOC_ACCOUNT=$SLURM_ACCOUNT
 
-TRAINER=finetune
-#TRAINER=pretrain
-START_POINT=save/$TRAINER/0-0-0 #base-models/bert-base-uncased
-TOK_CKPOINT=base-models/bert-tokenizer
-if [ $TRAINER == finetune ]; then
-    ADD_DAT=./data/mse-aops-2021-data.pkl.tags.ids
-else
-    ADD_DAT=./data/mse-aops-2021-vocab.pkl
+DATA_DIR=data.$DATA_VER
+if [ ! -e $DATA_DIR ]; then
+    tarball=`mktemp`
+    wget https://vault.cs.uwaterloo.ca/s/$DATA_VER -O $tarball
+    tar xzf $tarball --one-top-level=$DATA_DIR --strip-components 1
 fi
 
-set -x
-srun --unbuffered python \
-    ./pya0/utils/transformer.py $TRAINER $START_POINT $TOK_CKPOINT $ADD_DAT \
+srun --unbuffered \
+    python ./pya0/utils/transformer.py $TRAINER $START_POINT $TOK_CKPOINT $EXTRA_DAT \
     --cluster tcp://$(hostname):8921 \
-    --shards_list data/shards-for-$TRAINER.txt \
-    --batch_size $((2 * 2 * 10)) --save_fold 4 --epochs 20
+    --shards_list $SHARDS_LIST \
+    --batch_size $(($N_NODE * $N_GPUS * $DEV_BSIZE)) \
+    --save_fold 5 --epochs $EPOCHS $EXTRA_ARG
 
 # Other example usages
 #srun python pytorch-test-v2.py tcp://$(hostname):8921
