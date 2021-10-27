d7f99b13ff72805175eed23048973ad7f47af114
t-k- <clock126@126.com>
2021-10-27 04:00:13 -0400

    fixup

--- cc-install-conda-pkg.sh
+++ cc-install-conda-pkg.sh
@@ -1,3 +1,4 @@
 export PYTHONPATH=""
 export PIP_CONFIG_FILE=""
-conda create --name base_pkg --file conda-requirements.txt
+conda create --name base_pkg --file conda-requirements.txt -c huggingface -c conda-forge -c pytorch
+pip install pyjnius onnxruntime fire
