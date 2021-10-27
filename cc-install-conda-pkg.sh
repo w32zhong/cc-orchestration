de385f34203f2e2a27857dfd795a2fb2a5da0878
t-k- <clock126@126.com>
2021-10-27 04:21:10 -0400

    fixes

--- cc-install-conda-pkg.sh
+++ cc-install-conda-pkg.sh
@@ -1,4 +1,10 @@
 export PYTHONPATH=""
 export PIP_CONFIG_FILE=""
-conda create --name base_pkg --file conda-requirements.txt -c huggingface -c conda-forge -c pytorch
+conda create --yes --name base_pkg python=3.8.11
+conda activate base_pkg
+conda install --yes pytorch=1.10.0 cudatoolkit=10.1.243 -c pytorch
+conda install --yes faiss-gpu=1.6.5 -c conda-forge 
+conda install --yes openjdk=11 -c conda-forge
+conda install --yes pandas scikit-learn
+pip install transformers==4.9.2
 pip install pyjnius onnxruntime fire
