32abfa72d70a0474575fc82f8379c88507f3e324
t-k- <clock126@126.com>
2021-11-03 04:35:10 -0400

    fix msmarco_utils.sh

--- msmarco_utils.sh
+++ msmarco_utils.sh
@@ -3,7 +3,7 @@ COLLECTION=/project/6016715/w32zhong/msmarco-passage-collection/collection.tsv
 EVAL='python /home/w32zhong/anserini/tools/scripts/msmarco/msmarco_passage_eval.py'
 TOPICS=/project/6016715/w32zhong/msmarco-passage-collection/queries.*.tsv
 
-function visualize_query_and_hits() {
+function visualize_for_msmacrofile() {
 	qid=$1
 	cat $TOPICS | grep "^$qid\\b"
 	[ -z $2 ] && return;
@@ -14,9 +14,31 @@ function visualize_query_and_hits() {
 	echo '-----'
 	while read line; do
 		docid=$(echo $line | awk '{print $2}')
-		score=$(echo $line | awk '{print $3}')
+		rank=$(echo $line | awk '{print $3}')
+		echo -ne "$rank \t"
 		cat $COLLECTION | grep "^$docid\\b"
 		cat $QRELS | grep --color "$qid[[:blank:]]0[[:blank:]]$docid"
+		echo '-----'
+	done < $tmpfile
+}
+
+function visualize_for_trecfile() {
+	qid=$1
+	cat $TOPICS | grep "^$qid\\b"
+	[ -z $2 ] && return;
+	runfile=$2
+	topk=${3-3}
+	tmpfile=`mktemp`
+	rank=1
+	cat $runfile | grep "^$qid\\b" | sort -k4 -n | head -$topk > $tmpfile
+	echo '-----'
+	while read line; do
+		docid=$(echo $line | awk '{print $3}')
+		score=$(echo $line | awk '{print $5}')
+		echo -ne "$rank \t $score \t"
+		cat $COLLECTION | grep "^$docid\\b"
+		cat $QRELS | grep --color "$qid[[:blank:]]0[[:blank:]]$docid"
+		let 'rank=rank+1'
 		echo '-----'
 	done < $tmpfile
 }
@@ -28,9 +50,9 @@ case $1 in
 		sorted_qrels=`mktemp`
 		subset_qrels=`mktemp`
 		cat $runfile | awk '{print $1}' | sort -u > $run_qids
-		cat $QRELS | sort -k 1 | uniq > $sorted_qrels
-		join $run_qids $sorted_qrels 2> /dev/null > $subset_qrels
-		wc -l $sorted_qrels $run_qids $subset_qrels
+		cat $QRELS | sort -k 1,1 | uniq > $sorted_qrels
+		join $run_qids $sorted_qrels > $subset_qrels
+		wc -l $run_qids $sorted_qrels $subset_qrels
 		$EVAL $subset_qrels $runfile
 	;;
 
@@ -41,8 +63,13 @@ case $1 in
 		set +x
 	;;
 
+
 	debug)
-		visualize_query_and_hits $2 $3 $4
+		visualize_for_msmacrofile $2 $3 $4
+	;;
+
+	debug-trec)
+		visualize_for_trecfile $2 $3 $4
 	;;
 
 	query)
