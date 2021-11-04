248e968b1a1c9210424458063a690680e8275937
Wei <clock126@126.com>
2021-11-03 23:07:30 -0400

    Update cc-install-conda-pkg.sh

--- cc-install-conda-pkg.sh
+++ cc-install-conda-pkg.sh
@@ -3,9 +3,8 @@ export PIP_CONFIG_FILE=""
 #conda create --yes --name pyserini python=3.8.10
 #conda activate pyserini
 
-conda install --yes -c huggingface transformers
 conda install --yes -c conda-forge faiss-gpu=1.6.5
 conda install --yes pytorch=1.10.0 cudatoolkit=11.3 -c pytorch
 conda install --yes openjdk=11 -c conda-forge
 conda install --yes pandas tqdm
-pip install pyjnius onnxruntime fire scikit-learn
+pip install pyjnius onnxruntime fire scikit-learn transformers
