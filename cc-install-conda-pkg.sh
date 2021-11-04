3dc0f8c2324700f8bbd25fec6eadcec6cfd8c292
t-k- <clock126@126.com>
2021-11-03 23:00:50 -0400

    update

--- cc-install-conda-pkg.sh
+++ cc-install-conda-pkg.sh
@@ -7,6 +7,5 @@ conda install --yes -c huggingface transformers
 conda install --yes -c conda-forge faiss-gpu=1.6.5
 conda install --yes pytorch=1.10.0 cudatoolkit=11.3 -c pytorch
 conda install --yes openjdk=11 -c conda-forge
-conda install --yes pandas scikit-learn
-conda install tqdm
-pip install pyjnius onnxruntime fire
+conda install --yes pandas tqdm
+pip install pyjnius onnxruntime fire scikit-learn
