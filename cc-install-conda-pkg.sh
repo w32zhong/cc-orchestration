70dd5861651acd0e264d0334e26be3b41aeaf33a
Wei <clock126@126.com>
2021-11-05 17:41:44 -0400

    Update cc-install-conda-pkg.sh

--- cc-install-conda-pkg.sh
+++ cc-install-conda-pkg.sh
@@ -2,7 +2,7 @@ export PYTHONPATH=""
 export PIP_CONFIG_FILE=""
 #conda create --yes --name py38 python=3.7.10
 #conda activate py38
-conda install --yes pytorch=1.8.1 cudatoolkit=10.1 -c pytorch
+conda install --yes pytorch cudatoolkit=11.1 -c pytorch
 pip install faiss-gpu==1.6.5
 conda install --yes openjdk=11 -c conda-forge
 pip install transformers==4.9.2
