c5789510f703388b9f74b9ab026c4d7c994ed8a1
t-k- <clock126@126.com>
2021-11-05 01:06:37 -0400

    update

--- sbatch-pyserini-encode.sh
+++ sbatch-pyserini-encode.sh
@@ -2,7 +2,7 @@
 #SBATCH --nodes=1           # total nodes
 #SBATCH --gres=gpu:1        # how many GPUs per node
 #SBATCH --cpus-per-task=4   # Cores proportional to GPUs: 6 on Cedar, 16 on Graham.
-#SBATCH --mem=64gb          # Memory proportional to GPUs: 32000 Cedar, 64000 Graham.
+#SBATCH --mem=128gb         # Memory proportional to GPUs: 32000 Cedar, 64000 Graham.
 #SBATCH --time=2-02:10      # 2 days and 2 hours and 10 minutes
 #SBATCH --output=job-%j-%N.out
 set -x
@@ -15,12 +15,16 @@ export SBATCH_ACCOUNT=$SLURM_ACCOUNT
 export SALLOC_ACCOUNT=$SLURM_ACCOUNT
 
 cd pyserini
-srun --unbuffered python -m pyserini.encode \
-	input --corpus ../msmarco-passage-corpus \
-	encoder --encoder ../encoders/colbert_distil_128 \
-	--tokenizer ../encoders/tokenizer-distilbert-base-uncased \
-	--batch 64 --fp16 --device cuda:0 \
-	output --embeddings ../msmarco-passage-index-$SLURM_JOBID
+#srun --unbuffered python -m pyserini.encode \
+#	input --corpus ../msmarco-passage-corpus \
+#	encoder --encoder ../encoders/colbert_distil_128 \
+#	--tokenizer ../encoders/tokenizer-distilbert-base-uncased \
+#	--batch 64 --fp16 --device cuda:0 \
+#	output --embeddings ../msmarco-passage-index-$SLURM_JOBID
+
+srun --unbuffered python -m pyserini.index._colbert \
+	--index_path ../msmarco-passage-index-138014 \
+	--use_gpu=False create_compressed_faiss_index
 
 # Other example usages
 #salloc --nodes=1 --gres=gpu:1 --cpus-per-task=2 --time=0-01:10 --mem=32gb
