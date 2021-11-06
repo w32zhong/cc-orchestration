0a409986ee7a1b0f29c2306dacd986f61472ef2d
t-k- <clock126@126.com>
2021-11-06 14:13:18 -0400

    update

--- sbatch-pyserini-search.sh
+++ sbatch-pyserini-search.sh
@@ -13,7 +13,7 @@ export SBATCH_ACCOUNT=$SLURM_ACCOUNT
 export SALLOC_ACCOUNT=$SLURM_ACCOUNT
 
 COMMAND="$0 $@"
-SRCH_RANGE=${1-8_4_8} # or 8_0_4
+SRCH_RANGE=${1-10_5_10} # or 10_0_5
 
 cd pyserini
 srun --unbuffered python -m pyserini.dsearch \
