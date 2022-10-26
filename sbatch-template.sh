176ca43ce1e8f89fd7571b4a306756dd33b53321
t-k- <clock126@126.com>
2022-10-26 11:05:17 -0400

    fixup and added A100 cluster for condensor pretrain.

--- sbatch-template.sh
+++ sbatch-template.sh
@@ -46,6 +46,48 @@ case $TRAINER-${SETUP} in
     TRAINER_ARGS=
     ;;
 
+   pretrain-for-newvocab-using-v3-data-a100)
+    DEV_BSIZE=30
+    SAVE_FOLD=1
+
+    DATA_VER=nzzsWr7Nsz6sjfW
+    START_POINT=bert-base-uncased
+    TOK_CKPOINT=bert-tokenizer
+    SHARDS_LIST=shards.txt
+    TEST_FILE=test.txt
+    TEST_CYCLE=100
+    CALL_ARGS="data.$DATA_VER/mse-aops-2021-vocab-v3.pkl"
+    TRAINER_ARGS="--architecture standard --warmup-epochs 1 --lr 1e-4"
+    ;;
+
+   pretrain-condenser-using-v3-allenv-a100)
+    DEV_BSIZE=12
+    SAVE_FOLD=1
+
+    DATA_VER=Y53g3oaE42pXm8o
+    START_POINT=bert-base-uncased
+    TOK_CKPOINT=bert-tokenizer
+    SHARDS_LIST=shards.txt
+    TEST_FILE=test.txt
+    TEST_CYCLE=100
+    CALL_ARGS="data.$DATA_VER/mse-aops-2021-vocab-v3-allenv.pkl"
+    TRAINER_ARGS="--architecture condenser --warmup-epochs 1 --lr 1e-4"
+    ;;
+
+   pretrain-cotmae-using-v3-allenv-a100)
+    DEV_BSIZE=12
+    SAVE_FOLD=1
+
+    DATA_VER=Y53g3oaE42pXm8o
+    START_POINT=bert-base-uncased
+    TOK_CKPOINT=bert-tokenizer
+    SHARDS_LIST=shards.txt
+    TEST_FILE=test.txt
+    TEST_CYCLE=100
+    CALL_ARGS="data.$DATA_VER/mse-aops-2021-vocab-v3-allenv.pkl"
+    TRAINER_ARGS="--architecture mae --warmup-epochs 1 --lr 1e-4"
+    ;;
+
    pretrain-for-newvocab-using-v3-data-a6000)
     DEV_BSIZE=38
     SAVE_FOLD=1
@@ -64,7 +106,7 @@ case $TRAINER-${SETUP} in
     DEV_BSIZE=16
     SAVE_FOLD=1
 
-    DATA_VER=9ap65wbmEJ6PZre
+    DATA_VER=Y53g3oaE42pXm8o
     START_POINT=bert-base-uncased
     TOK_CKPOINT=bert-tokenizer
     SHARDS_LIST=shards.txt
@@ -78,7 +120,7 @@ case $TRAINER-${SETUP} in
     DEV_BSIZE=16
     SAVE_FOLD=1
 
-    DATA_VER=9ap65wbmEJ6PZre
+    DATA_VER=Y53g3oaE42pXm8o
     START_POINT=bert-base-uncased
     TOK_CKPOINT=bert-tokenizer
     SHARDS_LIST=shards.txt
